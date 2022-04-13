88c71774a7fc6e0f063ffac4e0b047a90caa99ca
riscaldo <riccardo85ferrara@gmail.com>
2022-04-13 18:22:58 +0200

    add function to calculate the winner

--- src/index.js
+++ src/index.js
@@ -75,6 +75,28 @@ class Game extends React.Component {
     }
 }
 
+function calculateWinner(squares) {
+    //returns the winner as 'X', 'O' o null 
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
+
+
 // ========================================
 
 ReactDOM.render( <
