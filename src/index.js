23d5c1d9329e17b1f1cc2d23d9a2fbe1c38955f3
riscaldo <riccardo85ferrara@gmail.com>
2022-04-13 18:03:03 +0200

    save the value of each square in the square state

--- src/index.js
+++ src/index.js
@@ -4,20 +4,13 @@ import './index.css';
 
 // this component is one of the 9 squares in a board
 class Square extends React.Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            value: null
-        }
-    }
-
     render() {
         return ( 
             <button 
                 className = "square" 
-                onClick={() => {this.setState({value: 'X'})}} 
+                onClick={() => { this.props.onClick() }} 
             > 
-                { this.state.value } 
+                { this.props.value } 
             </button>
         );
     }
@@ -31,6 +24,16 @@ class Board extends React.Component {
             squares: Array(9).fill(null) 
         }
     }
+
+    //this function save the value of each square in the square state
+    handleClick(i) {
+        const squares = this.state.squares.slice()
+        squares[i] = 'X'
+        this.setState({
+            squares: squares
+        })
+    }
+
     renderSquare(i) {
         return (
             <Square
