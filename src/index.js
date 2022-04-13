c6d9c73ee93399629a18ec7a14b49ae4f7b649a1
riscaldo <riccardo85ferrara@gmail.com>
2022-04-13 17:27:29 +0200

    show X when click on a square

--- src/index.js
+++ src/index.js
@@ -4,10 +4,17 @@ import './index.css';
 
 // this component is one of the 9 squares in a board
 class Square extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            value: null
+        }
+    }
+
     render() {
         return ( 
-            <button className = "square" onClick={function () {console.log('click')}} > 
-                { this.props.value } 
+            <button className = "square" onClick={() => {this.setState({value: 'X'})}} > 
+                { this.state.value } 
             </button>
         );
     }
